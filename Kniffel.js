5407fb93ddc76caf28055222ffc6656f4fefab25
michaelmdk1 <michaelmdkuebler@gmail.com>
2012-06-23 16:00:59 +0200

    wuerfeln geht

--- Kniffel.js
+++ Kniffel.js
@@ -2,12 +2,13 @@ var counter = 0
 var allDice = [0, 0, 0, 0, 0];
 var wurf = function() {
     for(i=0;i<5;i++){
-		if(document.dice.elements[i].checked){
+		if(!document.dice.elements[i].checked){
 			allDice[i] = Math.floor(Math.random()*6+1);
 			}
 		changeText("dice"+(i+1),allDice[i]);
        }
-       counter +=1
+       counter +=1;
+       changeText("counter", counter);
        
 };
 var changeText = function(id, text){

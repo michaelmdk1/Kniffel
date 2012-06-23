f0ceca401f0f8aa80174092da066e15f7b87b05d
rsdk <rnkblr@gmail.com>
2012-06-23 17:05:20 +0200

    funktion groses

--- Kniffel.js
+++ Kniffel.js
@@ -14,14 +14,16 @@ var wurf = function() {
 var changeText = function(id, text){
 	document.getElementById(id).innerHTML=text;
 };
+
 var one = function() {
-     var einser = 0
+     var einser = 0;
      for(i=0;i<5;i++){
 		if(allDice[i]===1){
 			einser += 1;
 		}
 	 changeText("p1_one", einser);
 };
+
 var two = function() {
        
 };
@@ -56,5 +58,18 @@ var kleines = function() {
        
 };
 var groses = function() {
-       
+	var new_ad = allDice.slice();
+	new_ad.sort();
+	if(new_ad[0] > 2){return}
+	else {
+			for(i=new_ad[0];i<new_ad[0]+3;i++)
+				{
+					if(new_ad[i]+1 !== new_ad[i+1])
+					{
+						changeText("p1_groses", "Nope");
+					}
+				}
+		changeText("p1_groses", 40);
+		}
+	 
 };
